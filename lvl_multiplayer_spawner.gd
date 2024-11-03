df21949cfbbb8ff6b201b3f23bb35f69b8d7a829
BasilYes <basilyevs@gmail.com>
2024-11-03 10:34:17 +0300

    fix no const bug

--- lvl_multiplayer_spawner.gd
+++ lvl_multiplayer_spawner.gd
@@ -3,12 +3,10 @@ extends MultiplayerSpawner
 
 func _enter_tree() -> void:
 	spawn_path = "."
-	var scene_list: Array[String] = ProjectSettings.get_setting(EMEditorPlugin.SCENE_LIST,
+	var scene_list: Array[String] = ProjectSettings.get_setting("easy_multiplayer/replicable_scenes",
 			Array([], TYPE_STRING, "", null))
-	var server_scene: String = ProjectSettings.get_setting(EMEditorPlugin.SERVER_SCENE, "")
+	var server_scene: String = ProjectSettings.get_setting("easy_multiplayer/server_scene", "")
 	if not server_scene in scene_list:
 		scene_list.append(server_scene)
 	for i in scene_list:
 		add_spawnable_scene(i)
-	for i in get_spawnable_scene_count():
-		print(get_spawnable_scene(i))
